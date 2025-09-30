379f19586f54f00f613edf98f85cc4eef92bddb6
Virgill_e <virgile.bigare@gmail.com>
2025-09-30 11:22:04 +0200

    day2 part 2

--- src/main/java/com/virgille/exercice/Day02.java
+++ src/main/java/com/virgille/exercice/Day02.java
@@ -3,6 +3,7 @@ package com.virgille.exercice;
 import com.virgille.Solution;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day02 implements Solution {
@@ -11,41 +12,70 @@ public class Day02 implements Solution {
     public String part1(List<String> input) {
         int solve = 0;
 
-        for(String s : input){
-            String[] parts = s.split(" ");
-            int num1 = Integer.parseInt(parts[0]);
-            int num2 = Integer.parseInt(parts[1]);
-            boolean isSafe = false;
-            if(num1 < num2){
-                for(int i = 0; i<parts.length-1; i++){
-                    int numTemp1 = Integer.parseInt(parts[i]);
-                    int numTemp2 = Integer.parseInt(parts[i+1]);
-                    isSafe = numTemp1 < numTemp2 && Math.abs(numTemp1-numTemp2) <=3;
-                    if(!isSafe){
-                        break;
-                    }
-                }
-            } else if (num1>num2) {
-                for(int i = 0; i<parts.length-1; i++){
-                    int numTemp1 = Integer.parseInt(parts[i]);
-                    int numTemp2 = Integer.parseInt(parts[i+1]);
-                    isSafe = numTemp1 > numTemp2 && Math.abs(numTemp1-numTemp2) <=3;
-                    if(!isSafe){
-                        break;
-                    }
-                }
-            }
-
-            if(isSafe){
+        for (String s : input) {
+            List<String> parts = Arrays.asList(s.split(" "));
+            if (isSafe(parts)) {
                 solve++;
             }
-
         }
         return String.valueOf(solve);
     }
 
     @Override
     public String part2(List<String> input) {
-        return "";
+        int solve = 0;
+
+        for (String s : input) {
+            List<String> parts = Arrays.asList(s.split(" "));
+            if (isSafeWithRemove(parts)) {
+                solve++;
+            }
+        }
+        return String.valueOf(solve);
+    }
+
+    private boolean isSafe(List<String> parts) {
+        int num1 = Integer.parseInt(parts.get(0));
+        int num2 = Integer.parseInt(parts.get(1));
+        boolean isSafe = false;
+        if (num1 == num2) {
+            return false;
+        }
+
+        if (num1 < num2) {
+            for (int i = 0; i < parts.size() - 1; i++) {
+                int numTemp1 = Integer.parseInt(parts.get(i));
+                int numTemp2 = Integer.parseInt(parts.get(i+1));
+                isSafe = numTemp1 < numTemp2 && Math.abs(numTemp1 - numTemp2) <= 3;
+                if (!isSafe) {
+                    return false;
+                }
+            }
+        } else {
+            for (int i = 0; i < parts.size() - 1; i++) {
+                int numTemp1 = Integer.parseInt(parts.get(i));
+                int numTemp2 = Integer.parseInt(parts.get(i + 1));
+                isSafe = numTemp1 > numTemp2 && Math.abs(numTemp1 - numTemp2) <= 3;
+                if (!isSafe) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isSafeWithRemove (List<String> parts) {
+        if(isSafe(parts)) {
+            return true;
+        }else {
+            for (int i = 0; i < parts.size(); i++) {
+                List<String> tempParts = new ArrayList<>(parts);
+                tempParts.remove(i);
+                if (isSafe(tempParts)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
